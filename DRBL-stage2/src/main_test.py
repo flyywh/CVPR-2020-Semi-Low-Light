6da8c76943c514da459d1691b3d95cafe754a869
Yang Wenhan <yangwenhan@pku.edu.cn>
2020-08-22 15:20:54 +0800

    Update main_test.py

--- DRBL-stage2/src/main_test.py
+++ DRBL-stage2/src/main_test.py
@@ -14,15 +14,15 @@ if checkpoint.ok:
     loader = data.Data(args)
 
     my_model = model.Model(args, checkpoint)
-    my_model.model.load_state_dict(torch.load('../experiment/test_201_dlprior_v1/model/model_10.pt'))
+    my_model.model.load_state_dict(torch.load('./pretrain/model_s2.pt'))
 
     args.model = 'RECOMPOSE'
     my_recomp = model.Model(args, checkpoint)
-    my_recomp.model.load_state_dict(torch.load('../experiment/test_201_dlprior_v1/model/model_R_10.pt'))
+    my_recomp.model.load_state_dict(torch.load('./pretrain/model_R_s2.pt'))
 
     args.model = 'DISCRIMINATOR'
     my_dis = model.Model(args, checkpoint)
-    my_dis.model.load_state_dict(torch.load('../experiment/test_201_dlprior_v1/model/model_D_10.pt'))
+    my_dis.model.load_state_dict(torch.load('./pretrain/model_D_s2.pt'))
 
     args.n_colors = 3
 
